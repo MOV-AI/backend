8973d5a65e84bc2c37fcdeeb5b61c77b61cc7141
duartecoelho <duarte@mov.ai>
2022-04-01 11:24:52 +0100

    switch pipelines to templates, plus prepare packaging

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@ with open("README.md", "r") as fh:
 # The name of the package is the one to be used in runtime.
 # The 'install_requires' is where you specify the package dependencies of your package. They will be automaticly installed, before your package.  # noqa: E501
 setuptools.setup(
-    name="dummy-component",
-    version="1.0.0-2",
-    author="DevOps team",
-    author_email="devops@mov.ai",
+    name="backend",
+    version="1.0.0-0",
+    author="Backend team",
+    author_email="backend@mov.ai",
     description="Dummy description",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/MOV-AI/repository-template-python-component",
+    url="https://github.com/MOV-AI/backend",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=["Programming Language :: Python :: 3"],
     install_requires=[],
-    entry_points={"console_scripts": ["my-command = DummyComponent.handler:handle"]},
+    entry_points={},
 )
---
+++ tests/test_suite.py
@@ -0,0 +1,5 @@
+import unittest
+
+class TestSuite(unittest.TestCase):
+    def test_handler_executer_forward(self):
+        assert(True)
