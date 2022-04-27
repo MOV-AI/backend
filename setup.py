f9faad4b0c8534a0f57dfda3c9e10b6df81a4e49
Automatic <test@test>
2022-04-27 10:21:14 +0000

    Automatic Bump of build version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requirements = [
 
 setuptools.setup(
     name="backend",
-    version="1.0.0-19",
+    version="1.0.0-20",
     author="Backend team",
     author_email="backend@mov.ai",
     description="Dummy description",
