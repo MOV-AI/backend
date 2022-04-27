ac0169c54b3f57a0b490fb60c33f915b24910e52
Mograbi <mograbi.moawiya@gmail.com>
2022-04-27 11:44:53 +0300

    added enterprise check, updated versions

--- setup.py
+++ setup.py
@@ -6,6 +6,20 @@ with open("README.md", "r") as fh:
 # TODO Adapt your project configuration to your own project.
 # The name of the package is the one to be used in runtime.
 # The 'install_requires' is where you specify the package dependencies of your package. They will be automaticly installed, before your package.  # noqa: E501
+
+requirements = [
+    "aiohttp==3.6.2",
+    "aiohttp-cors==0.7.0",
+    "requests==2.22.0",
+    "deepdiff==4.0.9",
+    "PyYAML==5.1.2",
+    "rospkg==1.3.0",
+    "movai_core_shared==1.0.0.8",
+    "dal==1.0.0.25",
+    "movai_core_enterprise==1.0.0.13",
+    "gd_node==1.0.0.7"
+]
+
 setuptools.setup(
     name="backend",
     version="1.0.0-15",
@@ -18,10 +32,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=["Programming Language :: Python :: 3"],
-    install_requires=[
-        "aiohttp==3.6.2", "aiohttp-cors==0.7.0", "requests==2.22.0",
-        "deepdiff==4.0.9", "PyYAML==5.1.2", "rospkg==1.3.0",
-        "movai_core_shared==1.0.0.8", "dal==1.0.0.23", "movai_core_enterprise==1.0.0.12", "gd_node==1.0.0.4"
-    ],
+    install_requires=requirements 
     entry_points={},
 )
