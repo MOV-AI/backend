62ae9cc32cf31dbeccaecafa2bcaddb7dc9d3488
Erez Zomer <erez@mov.ai>
2022-08-23 00:20:21 +0300

    role model

--- backend/models/role.py
+++ backend/models/role.py
@@ -10,72 +10,88 @@
 """
 
 from typing import Dict
-
-from dal.scopes import scopes
-
-from dal.models import Model
+from deprecated.api.core.acl import NewACLManager
+from movai_core_shared.exceptions import RoleAlreadyExist, RoleDoesNotExist
+from movai_core_shared.envvars import DEFAULT_ROLE_NAME
+from movai.data import scopes
+from dal.models.aclobject import AclObject
+from dal.models.remoteuser import RemoteUser
+from dal.models.internaluser import InternalUser
+from dal.models.model import Model
 
 
 class Role(Model):
-    """Role Model (only of name)"""
+    """ Role Model (only of name) """
 
-    # default __init__
+    @classmethod
+    def create(cls, name: str, resources: Dict) -> None:
+        """create a new Role object in DB
 
-    def create_permission(self, resource: str, permission: str) -> bool:
-        """Creates a new role permission
+        Args:
+            name (str): The name of the Role
+            resources (Dict): resources permissions map
 
-        Doesn't write on storage, call `.write()` to save
+        Raises:
+            RoleAlreadyExist: in case a Role with that name already exist.
         """
         try:
-            self.Resources[resource.capitalize()].append(permission.lower())
-        except KeyError:  # resource doesn't exist
-            return False
+            role = scopes().create(Role.__name__, name)
+            role.Label = name
+            role.update(resources)
+            return role
+        except ValueError:
+            error_msg = "The requested Role already exist"
+            cls.log.error(error_msg)
+            raise RoleAlreadyExist(error_msg)
 
-        return True
+    @classmethod
+    def create_default_role(cls):
+        if not Role.is_exist(DEFAULT_ROLE_NAME):
+            resources = NewACLManager.get_permissions()
+            default_role = cls.create(DEFAULT_ROLE_NAME, resources)
+        else:
+            default_role = Role(DEFAULT_ROLE_NAME)
+        return default_role
 
-    def delete_permission(self, resource: str, permission: str) -> bool:
-        """Delete role permission
+    def update(self, resources: Dict) -> None:
+        """ Update role data
+        """
+        self.Resources = resources
+        self.write()
 
-        Doesn't write to storage, call `.write()` to save
+    @classmethod
+    def remove(cls, name: str) -> None:
+        """Removes a Role from DB.
+
+        Args:
+            name (str): The name of the Role to remove
+
+        Raises:
+            RoleDoesNotExist: In case there is no Role with that name.
         """
         try:
-            self.Resources[resource.capitalize()].remove(permission.lower())
+            RemoteUser.remove_role_from_all_users(name)
+            InternalUser.remove_role_from_all_users(name)
+            AclObject.remove_roles_from_all_objects(name)
+            role = Role(name)
+            scopes().delete(role)
         except KeyError:
-            # KeyError: no such key resource
-            return False
-        except ValueError:
-            # ValueError: permission not found on list (we don't care)
-            pass
-        return True
-
+            error_msg = "The requested Role does not exist"
+            cls.log.error(error_msg)
+            raise RoleDoesNotExist(error_msg)
+        
     @staticmethod
-    def create(*, name: str, resources: Dict) -> bool:
-        """Create a new role"""
-        try:
-            name = name.lower()
-            role = scopes().create("Role", name)
-            role.Label = name
-            role.Resources = resources
-            role.write()
-        except ValueError:
-            # ValueError: scope already exists
-            return False
-        return True
+    def list_roles_names() -> list:
+        """Retunns a list with all Roles exist in the system.
 
-    @staticmethod
-    def update(*, name: str, resources: Dict) -> bool:
-        """Reset role data"""
-        try:
-            name = name.lower()
-            role = scopes.from_path(name, scope="Role")
-            role.Label = name
-            role.Resources = resources
-            role.write()  # or shall we not write?
-        except KeyError:
-            # KeyError: scope does not exist
-            return False
-
-        return True
+        Returns:
+            list: containing the name of the current Roles.
+        """
+        roles_names = []
+        for obj in scopes().list_scopes(scope='Role'):
+            role_name = str(obj['ref'])
+            roles_names.append(role_name)
+        return roles_names
 
 
-Model.register_model_class("Role", Role)
+Model.register_model_class('Role', Role)
