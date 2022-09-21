ffde8d7e5ea474ff5789667f346ba093c5516800
Erez Zomer <erez@mov.ai>
2022-09-21 07:16:38 +0300

    backend models moved to dal: User, Role, BaseUser, InternalUser, RemoteUser

--- backend/models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-   Copyright (C) Mov.ai  - All Rights Reserved
-   Unauthorized copying of this file, via any medium is strictly prohibited
-   Proprietary and confidential
-
-   Developers:
-   - Erez Zomer  (erez@mov.ai) - 2022
-"""
