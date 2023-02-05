d4bfec06772d398118d6e20ce4ac44a07ce74e32
Moawiya Mograbi <mograbi.moawiya@gmail.com>
2023-02-05 18:48:30 +0200

    used body for notifications

--- backend/endpoints/api/v2/notifications.py
+++ backend/endpoints/api/v2/notifications.py
@@ -10,43 +10,47 @@
    in the Message Server
 """
 from typing import List
+from urllib import parse
 from aiohttp import web, web_request
 from backend.http import WebAppManager
-from .base import BaseWebAp
+from backend.endpoints.api.v2.base import BaseWebApp
+
+# from .base import BaseWebAp
 from movai_core_shared.core.message_client import MessageClient
 from movai_core_shared.envvars import MESSAGE_SERVER_BIND_ADDR
 from movai_core_shared.consts import NOTIFICATIONS_HANDLER_MSG_TYPE
 import jsonpickle
 
 
-def get_emails(request: web.Request):
+async def get_emails(request: web.Request):
     return web.json_response([], headers={"Server": "Movai-server"})
 
 
-def send_email(request: web.Request):
+async def send_email(request: web.Request):
     client = MessageClient(MESSAGE_SERVER_BIND_ADDR)
-    recipients = parse.unquote(request.match_info["recipients"])
-    subject = parse.unquote(request.match_info["subject"])
-    body = parse.unquote(request.match_info["body"])
+    body = await request.json()
+    recipients = body["recipients"]
+    subject = body["subject"]
+    body = body["body"]
     subject = None
     attachment_path = None
     attachment_data = None
-    if "attachment_path" in request:
-        attachment_path = parse.unquote(request.match_info["attachment_path"])
-    if "subject" in request:
-        subject = parse.unquote(request.match_info["subject"])
+    if "attachment_path" in body:
+        attachment_path = body["attachment_path"]
+    if "subject" in body:
+        subject = body["subject"]
     if attachment_path:
         with open(attachment_path, "rb") as f:
             attachment_data = jsonpickle.encode(f.read())
 
     data = {
-                "req_type": NOTIFICATIONS_HANDLER_MSG_TYPE
-                "req_data": {
-                    "recipients": recipients,
-                    "notification_type": "smtp",
-                    "body": body,
-                }
-            }
+        "req_type": NOTIFICATIONS_HANDLER_MSG_TYPE,
+        "req_data": {
+            "recipients": recipients,
+            "notification_type": "smtp",
+            "body": body,
+        },
+    }
 
     if subject:
         data.update({"subject": subject})
@@ -58,45 +62,38 @@ def send_email(request: web.Request):
     return web.json_response({"result": res}, headers={"Server": "Movai-server"})
 
 
-def send_sms(request: web.Request):
+async def send_sms(request: web.Request):
     client = MessageClient(MESSAGE_SERVER_BIND_ADDR)
-    recipients = parse.unquote(request.match_info["recipients"])
-    msg = parse.unquote(request.match_info["msg"])
+    body = await request.json()
+    recipients = body["recipients"]
+    msg = body["msg"]
     data = {
-                "req_type": NOTIFICATIONS_HANDLER_MSG_TYPE
-                "req_data": {
-                    "notification_type": "sms"
-                    "recipients": recipients,
-                    "msg": msg
-                }
-            }
+        "req_type": NOTIFICATIONS_HANDLER_MSG_TYPE,
+        "req_data": {"notification_type": "sms", "recipients": recipients, "msg": msg},
+    }
 
     res = client.send_request(NOTIFICATIONS_HANDLER_MSG_TYPE, data)
 
     return web.json_response({"resutl": res}, headers={"Server": "Movai-server"})
 
 
-def send_user_notifications(request: web.Request):
+async def send_user_notifications(request: web.Request):
     client = MessageClient(MESSAGE_SERVER_BIND_ADDR)
-    msg = parse.unquote(request.match_info["msg"])
-    fleet = parse.unquote(request.match_info["fleet"])
-    robot_name = parse.unquote(request.match_info["robot_name"])
-    robot_id = parse.unquote(request.match_info["robot_id"])
-    service = parse.unquote(request.match_info["service"])
+    body = await request.json()
 
     data = {
-                "req_type": NOTIFICATIONS_HANDLER_MSG_TYPE
-                "req_data": {
-                    "notification_type": "user"
-                    "msg": msg,
-                    "tags": {
-                        "fleet": fleet,
-                        "robot_id": robot_id,
-                        "robot_name": robot_name,
-                        "service": service
-                    }
-                }
-            }
+        "req_type": NOTIFICATIONS_HANDLER_MSG_TYPE,
+        "req_data": {
+            "notification_type": "user",
+            "msg": body["msg"],
+            "tags": {
+                "fleet": body["fleet"],
+                "robot_id": body["robot_id"],
+                "robot_name": body["robot_name"],
+                "service": body["service"],
+            },
+        },
+    }
 
     res = client.send_request(NOTIFICATIONS_HANDLER_MSG_TYPE, data)
 
@@ -117,7 +114,7 @@ class NotificationsAPI(BaseWebApp):
             web.get(r"/emails", get_emails),
             web.post(r"/sms", send_sms),
             web.post(r"/user", send_user_notifications),
-            web.post(r"/email", send_email)
+            web.post(r"/email", send_email),
         ]
 
 
