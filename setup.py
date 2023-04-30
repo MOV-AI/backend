f6bc39c16c7f57f03f21e19662343f3542f96250
Dor.M <dor@mov.ai>
2023-04-30 15:03:11 +0300

    fixed version on setup

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ requirements = [
     "miracle-acl==0.0.4.post1",
     "PyYAML==5.1.2",
     "requests==2.22.0",
+    "movai-core-shared==2.4.1.10",
+    "data-access-layer==2.4.1.17",
     "gd-node==2.4.1.12",
 ]
 
