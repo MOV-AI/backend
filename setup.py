d43a135df8729b462b5b423eed7626e034dd24c7
Moawiya Mograbi <mograbi.moawiya@gmail.com>
2023-05-25 14:39:07 +0300

    updated dal version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
     "PyYAML==5.1.2",
     "requests==2.22.0",
     "movai-core-shared==2.4.1.13",
-    "data-access-layer==2.4.1.21",
+    "data-access-layer==2.4.1.22",
     "gd-node==2.4.1.15",
 ]
 
