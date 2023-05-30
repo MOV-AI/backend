f8955d4ffe1a9a3a6c297fc4c34281410aaaad37
Erez Zomer <erez@mov.ai>
2023-05-30 20:18:30 +0300

    helper funtions for restapi

---
+++ backend/helpers/rest_helpers.py
@@ -0,0 +1,24 @@
+from aiohttp import web
+
+def deprecate_endpoint() -> None:
+    """This is a helper function to deprecate unused functions
+
+    Raises:
+        web.HTTPForbidden
+    """
+    raise web.HTTPForbidden(reason="This endpoint is deprecated")
+
+def fetch_request_params(request: dict) -> dict:
+    """fetches the params from the request and returns them in a dictionary.
+
+    Args:
+        request (dict): The request with the params.
+
+    Returns:
+        dict: A dictionary of params and their value.
+    """
+    params = {}
+    for param in request.query_string.split("&"):
+        name, value = param.split("=")
+        params[name] = value
+    return params
