b2a364ca412e091341287fb03e6d19c87fa10048
Erez Zomer <erez@mov.ai>
2023-06-05 11:11:42 +0300

    add docstring

--- backend/core/log_streamer/logs_streamer.py
+++ backend/core/log_streamer/logs_streamer.py
@@ -9,6 +9,8 @@
    Developers:
    - Erez Zomer (erez@mov.ai) - 2023
 """
+import logging
+
 from movai_core_shared.core.zmq_server import ZMQServer
 from movai_core_shared.envvars import LOG_STREAMER_BIND_ADDR
 from movai_core_shared.logger import Log
@@ -18,22 +20,29 @@ from movai_core_shared.messages.log_data import LogRequest
 class LogsStreamer(ZMQServer):
 
     def __init__(self, debug: bool = False) -> None:
+        """Initializes the object.
+
+        Args:
+            debug (bool, optional): if True, will show debug logs while running ZMQServer
+        """
         super().__init__(self.__class__.__name__, LOG_STREAMER_BIND_ADDR, debug)
-        self._logger = Log.get_logger(self.__class__.__name__)
+        self._logger = logging.getLogger(self.__class__.__name__)
         self._clients = {}
         self.init_server()
         
     async def handle_request(self, request: dict) -> dict:
+        """Implements the handle_request function for ZMQServer.
+
+        Args:
+            request (dict): A request witho logs.
+
+        Returns:
+            dict: response
+        """
         try:
             log_msg = LogRequest(**request)
-            print(log_msg.req_data.log_fields.message)
-            #for client in self._clients:
-            #    client.send_msg(request.req_data.log_fields.message)
+            print(f"{self.__class__.__name__}: {log_msg.req_data.log_fields.message}")
             return {}
         except Exception as error:
             self._logger.error(str(error))
             return {}
-
-
-log_streamer = LogsStreamer()
-log_streamer.run()
