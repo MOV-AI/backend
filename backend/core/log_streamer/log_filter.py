2edcb1e85caa7b53b0808fa6e744e3dac1224534
Erez Zomer <erez@mov.ai>
2023-06-07 15:48:15 +0300

    add docstring to LogFilter

--- backend/core/log_streamer/log_filter.py
+++ backend/core/log_streamer/log_filter.py
@@ -4,28 +4,57 @@ from movai_core_shared.messages.log_data import LogRequest
 
 
 class ParamFilter(ABC):
+    """An abstract base class for various types of filter.
+    """
     def __init__(self, name: str) -> None:
+        """Ctor.
+
+        Args:
+            name (str): the name of the filter.
+        """
         super().__init__()
+        if not isinstance(name, str):
+            raise ValueError("name must be of type string")
         self._name = name
 
     @abstractmethod
     def filter_msg(self, msg: LogRequest):
+        """Abstract function for derived classes.
+
+        Args:
+            msg (LogRequest): The log msg to filter
+        """
         pass
 
     @property
     def name(self) -> str:
+        """property of the filter name.
+
+        Returns:
+            str: The name of the filter.
+        """
         return self._name
 
 class StrParam(ParamFilter):
-    def __init__(self, name: str, value) -> None:
+    def __init__(self, name: str, value: str) -> None:
+        """Ctor.
+
+        Args:
+            name (str): the name of the filter.
+        """
         super().__init__(name)
         if not isinstance(value, str):
-            raise ValueError("value must be an string")
+            raise ValueError("value must be of type string")
         self._value = value
 
 
 class IntParam(ParamFilter):
     def __init__(self, name: str, value) -> None:
+        """Ctor.
+
+        Args:
+            name (str): the name of the filter.
+        """        
         super().__init__(name)
         if not isinstance(value, int):
             raise ValueError("value must be an int")
@@ -34,60 +63,155 @@ class IntParam(ParamFilter):
 
 class ListParam(StrParam):
     def __init__(self, name: str, value) -> None:
+        """Ctor.
+
+        Args:
+            name (str): the name of the filter.
+        """        
         super().__init__(name, value)
         if not isinstance(value, list):
             self._value = [value]
 
 
 class RobotParam(ListParam):
+    """A filter for robot name.
+    """
     def __init__(self, value) -> None:
+        """Ctor.
+
+        Args:
+            name (str): the name of the filter.
+        """        
         super().__init__("robots", value)
 
-    def filter_msg(self, msg: LogRequest):
+    def filter_msg(self, msg: LogRequest) -> bool:
+        """filters a log messge based on robot name.
+
+        Args:
+            msg (LogRequest): The log message to filter.
+
+        Returns:
+            bool: True if log message can pass filter, False if not.
+        """
         return msg.req_data.log_tags.robot  in self._value
 
 
 class ServiceParam(ListParam):
+    """A filter for service name.
+    """
     def __init__(self, value) -> None:
+        """Ctor.
+
+        Args:
+            name (str): the name of the filter.
+        """
         super().__init__("services", value)
 
     def filter_msg(self, msg: LogRequest):
+        """filters a log messge based on service name.
+
+        Args:
+            msg (LogRequest): The log message to filter.
+
+        Returns:
+            bool: True if log message can pass filter, False if not.
+        """        
         return msg.req_data.log_tags.service in self._value
 
 
 class LevelParam(ListParam):
+    """A filter for level type.
+    """
     def __init__(self, value) -> None:
+        """Ctor.
+
+        Args:
+            name (str): the name of the filter.
+        """
         super().__init__("levels", value)
 
     def filter_msg(self, msg: LogRequest):
+        """filters a log messge based on level.
+
+        Args:
+            msg (LogRequest): The log message to filter.
+
+        Returns:
+            bool: True if log message can pass filter, False if not.
+        """
         return msg.req_data.log_tags.level in self._value
 
 
 class MessageParam(StrParam):
     def __init__(self, value) -> None:
+        """Ctor.
+
+        Args:
+            name (str): the name of the filter.
+        """
         super().__init__("message", value)
 
     def filter_msg(self, msg: LogRequest):
+        """filters a log messge based on message content.
+
+        Args:
+            msg (LogRequest): The log message to filter.
+
+        Returns:
+            bool: True if log message can pass filter, False if not.
+        """
         return msg.req_data.log_fields.message in self._value
 
 
 class FromDateParam(int):
+    """A filter for issue time is later than specific value.
+    """
     def __init__(self, value) -> None:
+        """Ctor.
+
+        Args:
+            name (str): the name of the filter.
+        """
         super().__init__("fromDate", value)
 
     def filter_msg(self, msg: LogRequest):
+        """filters a log messge if issue time is later than filter value.
+
+        Args:
+            msg (LogRequest): The log message to filter.
+
+        Returns:
+            bool: True if log message can pass filter, False if not.
+        """
         return msg.created >= self._value
     
 
 class ToDateParam(int):
+    """A filter for issue time is before than a specific value.
+    """
     def __init__(self, value) -> None:
+        """Ctor.
+
+        Args:
+            name (str): the name of the filter.
+        """
         super().__init__("toDate", value)
 
     def filter_msg(self, msg: LogRequest):
+        """filters a log messge if issue time is before filter value.
+
+        Args:
+            msg (LogRequest): The log message to filter.
+
+        Returns:
+            bool: True if log message can pass filter, False if not.
+        """
         return msg.created < self._value
 
 
 class LogFilter:
+    """A class for filtering a log msg through several types of filters.
+    """
     _filters_types = {
          "robots": RobotParam,
          "services": ServiceParam,
@@ -98,6 +222,8 @@ class LogFilter:
      }
 
     def __init__(self, **params):
+        """Ctor
+        """
         self._filters = []
         for key, val in params.items():
             if key in self._filters_types and val is not None:
@@ -105,7 +231,15 @@ class LogFilter:
                 self._filters.append(filter)
         
     def filter_msg(self, msg: LogRequest) -> bool:
-        for filter in self._filters():
+        """Checks that a LogRequest msg can pass the registered filters.
+
+        Args:
+            msg (LogRequest): The LogRequest from the message-server
+
+        Returns:
+            bool: True if can pass the filter, False otherwise.
+        """
+        for filter in self._filters:
             if not filter.filter_msg(msg):
                 return False
         return True
