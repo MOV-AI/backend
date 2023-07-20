a29e283f5c9c3616e944fbb033286be9239793bc
OttoMation-Movai <automation@mov.ai>
2023-07-20 09:44:59 +0000

    [skip actions] Automatic Bump of build version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requirements = [
 
 setuptools.setup(
     name="backend",
-    version="2.4.1-33",
+    version="2.4.1-34",
     author="Backend team",
     author_email="backend@mov.ai",
     description="Movai Backend Package",
