71be83055c9cfcebea7dbb9420276006007fae2f
M Mograbi <Mograbi@users.noreply.github.com>
2023-07-20 16:46:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
     "miracle-acl==0.0.4.post1",
     "PyYAML==6.0",
     "requests==2.22.0",
-    "movai-core-shared==3.4.1.22",
+    "movai-core-shared==2.4.1.22",
     "data-access-layer==2.4.1.32",
     "gd-node==2.4.1.16",
 ]
