1e39b4f6673806b3c6b6097ff4e319907c243772
Dor.M <dor@mov.ai>
2023-08-21 13:47:03 +0300

    updated to the latest shared

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
     "miracle-acl==0.0.4.post1",
     "PyYAML==6.0",
     "requests==2.22.0",
-    "movai-core-shared==2.4.1.24",
+    "movai-core-shared==2.4.1.26",
     "data-access-layer==2.4.1.35",
     "gd-node==2.4.1.17",
 ]
