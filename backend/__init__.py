6697a22d4aa6870388201a42a7f0a0d554f25b7e
erezz-mov-ai <94361921+erezz-mov-ai@users.noreply.github.com>
2023-11-06 01:36:36 +0200

    BP-984 - stream logs from message server to backend (#161)
    * LogServer
    
    * helper funtions for restapi
    
    * log streamer
    
    * changed the name to log_client
    
    * remove unnecessary imports
    
    * add docstring
    
    * restoring log_streamer operation
    
    * renaming LogsStreamer to LogStreamer
    
    * minor fixes
    
    * code review fixes
    
    * change naming of async subscriber to upper case
    
    * use async subscriber
    
    * remove unnecessary code
    
    * sonarcloud fixes
    
    ---------
    
    Co-authored-by: Otto Mation <92543170+OttoMation-Movai@users.noreply.github.com>

--- backend/__init__.py
+++ backend/__init__.py
@@ -8,7 +8,6 @@
 
    Module that implements the backend server application
 """
-
 import os
 from concurrent.futures import ThreadPoolExecutor
 
@@ -19,7 +18,7 @@ from dal.data.shared.vault import JWT_SECRET_KEY
 from gd_node.protocols.http.middleware import JWTMiddleware
 
 from backend import http
-from backend.core.log_streamer.logs_streamer import LogsStreamer
+from backend.core.log_streaming.log_streamer import LogStreamer
 from backend.endpoints.static import StaticApp
 from backend.endpoints import auth, ws, static
 from backend.endpoints.api import v1, v2
@@ -29,23 +28,24 @@ NODE_NAME = os.getenv("NODE_NAME", "backend")
 HTTP_HOST = os.getenv("HTTP_HOST", "0.0.0.0")
 HTTP_PORT = int(os.getenv("HTTP_PORT", "5004"))
 
+
 async def log_streamer(app: web.Application):
     """
     This function is made for context handling by aiohttp.
-    It will launch the log streamer in the background at startup and will close 
+    It will launch the log streamer in the background at startup and will close
     it at shutdown.
 
     Args:
         app (web.Application): The main application
     """
-    log_streamer = LogsStreamer()
-    app["log_streamer"] = log_streamer
-    log_streamer.run()
-    
+    streamer = LogStreamer()
+    app["log_streamer"] = streamer
+    streamer.start()
+
     yield
-    
-    log_streamer.stop()
-    log_streamer.close()
+
+    streamer.stop()
+
 
 async def root(_: web.Request) -> web.Response:
     """web app root"""
@@ -80,6 +80,7 @@ def main():
     main_app = web.Application()
     main_app["executor"] = ThreadPoolExecutor(max_workers=10)
     main_app.on_response_prepare.append(on_prepare)
+    main_app.cleanup_ctx.append(log_streamer)
 
     # prepare JWT middleware
     jwt_mw = JWTMiddleware(JWT_SECRET_KEY)
@@ -122,5 +123,5 @@ def main():
 
     # start the application
     # runs until interrupted
+
     web.run_app(main_app, host=HTTP_HOST, port=HTTP_PORT)
-    
