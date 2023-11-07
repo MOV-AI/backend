94796705ade5b1c966b203c8a00702188a8658e1
Erez Zomer <erez@mov.ai>
2023-11-07 14:39:53 +0200

    update requirements

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ requirements = [
     "PyYAML==6.0",
     "requests==2.28.2",
     "email-validator==2.0.0",
-    "movai-core-shared==2.4.1.36",
-    "data-access-layer==2.4.1.36",
+    "movai-core-shared==2.4.1.37",
+    "data-access-layer==2.4.1.37",
     "gd-node==2.4.1.21",
 ]
 
