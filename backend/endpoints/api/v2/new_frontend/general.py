153eac024fe2b28c66f85236048ea0fc8ff34e4c
Erez Zomer <erez@mov.ai>
2023-12-03 13:12:24 +0200

    remove  new_frontend

--- backend/endpoints/api/v2/new_frontend/general.py
+++
@@ -1,8 +0,0 @@
-class Responses:
-    def __init__(self) -> None:
-        self._responses = {}
-
-    def register_response(self, name, response):
-        if response is None:
-            raise ValueError("Response is None!")
-        self._responses[name] = response
--- backend/endpoints/api/v2/new_frontend/ide/get_ports_data.py
+++
@@ -1,2 +0,0 @@
-# Will send to NodeEditor the Packages and Messages options (for the 2nd and 3rd selectors)
-response = Message.fetch_portdata_api()
