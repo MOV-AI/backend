da71d2e113c48ce084578630372f05cf603b20ff
Erez Zomer <erez@mov.ai>
2023-12-04 13:29:39 +0200

    renaming fleetdashboard to fleetmanager

--- backend/endpoints/api/v1/frontend/__init__.py
+++ backend/endpoints/api/v1/frontend/__init__.py
@@ -1,15 +1,15 @@
+from .fleetmanager import fleetmanager_action_map, fleetmanager_enterprise_map
 from .ide import ide_action_map, ide_enterprise_map
-from .fleetdashboard import fleetdashoboard_action_map, fleetdashoboard_enterprise_map
 from .taskmanager import taskmanager_action_map, taskmanager_enterprise_map
 
 frontend_map = {
+    "fleetmanager": {
+        "action": fleetmanager_action_map,
+        "enterprise": fleetmanager_enterprise_map
+    },
     "ide": {
         "action": ide_action_map,
         "enterprise": ide_enterprise_map
-    },
-    "fleetdashboard": {
-        "action": fleetdashoboard_action_map,
-        "enterprise": fleetdashoboard_enterprise_map
     },
     "taskmanager": {
         "action": taskmanager_action_map,
