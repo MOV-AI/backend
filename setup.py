5a85fecfa93fc7c90c71866382e9eebde2f4ee81
Erez Zomer <erez@mov.ai>
2024-01-11 18:12:52 +0200

    use dal==2.5.0.8

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
     "email-validator==2.0.0",
     "pytz==2022.7.1",
     "movai-core-shared==2.5.0.10",
-    "data-access-layer==2.5.0.7",
+    "data-access-layer==2.5.0.8",
     "gd-node==2.5.0.7",
 ]
 
