fcba8537fcf83ddd9554509e0637945351cac994
andreparames <andreparames@users.noreply.github.com>
2024-10-08 12:03:04 +0100

    Upgrade DAL to 2.5.0.28

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
     "email-validator==2.0.0",
     "pytz==2022.7.1",
     "movai-core-shared==2.5.0.11",
-    "data-access-layer==2.5.0.9",
+    "data-access-layer==2.5.0.28",
     "gd-node==2.5.0.7",
 ]
 
