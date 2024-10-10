67d0fb8bd05336bea0652d37610daf0e82727b59
duartecoelhomovai <84720623+duartecoelhomovai@users.noreply.github.com>
2024-10-10 16:39:40 +0100

    Bump dal with remap fixes

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
     "email-validator==2.0.0",
     "pytz==2022.7.1",
     "movai-core-shared==2.5.0.18",
-    "data-access-layer==2.5.0.29",
+    "data-access-layer==2.5.0.30",
     "gd-node==2.5.0.18",
 ]
 
