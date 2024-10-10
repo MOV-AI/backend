1749da1f62a05df7a0fb4464d111fde271a9477c
duartecoelhomovai <84720623+duartecoelhomovai@users.noreply.github.com>
2024-10-10 16:26:10 +0100

    Rollout dal 2.5.0.29

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
     "email-validator==2.0.0",
     "pytz==2022.7.1",
     "movai-core-shared==2.5.0.18",
-    "data-access-layer==2.5.0.28",
+    "data-access-layer==2.5.0.29",
     "gd-node==2.5.0.18",
 ]
 
